41c62ca22f5aac0d9894834d9045a051cc3ff826
남의영 <ny2485@naver.com>
2024-04-05 02:30:59 +0900

    Initial commit

---
+++ src/main/java/spring/playground/SpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package spring.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundApplication.class, args);
+	}
+
+}
