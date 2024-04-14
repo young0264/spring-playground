e08056287da4a514fe80c8b628afae079275897b
남의영 <ny2485@naver.com>
2024-04-14 23:02:18 +0900

    create annotation to use  @Qualifier

--- src/main/java/spring/playground/basic/discount/RateDiscountPolicy.java
+++ src/main/java/spring/playground/basic/discount/RateDiscountPolicy.java
@@ -2,12 +2,14 @@ package spring.playground.basic.discount;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import spring.playground.basic.annotation.MainDiscountPolicy;
 import spring.playground.basic.member.Grade;
 import spring.playground.basic.member.Member;
 
 //정률 할인정책
 @Component
-@Primary //제외시 FixDiscountPolicy와 NoUniqueBeanDefinitionException 터짐
+//@Primary //제외시 FixDiscountPolicy와 NoUniqueBeanDefinitionException 터짐
+@MainDiscountPolicy // @Primary 또는 @Qualifier("mainDiscountPolicy") 대신 어노테이션 생성
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10; //10% 할인
