835d5db4d872a8f3c207752033b00297e6a4918a
남의영 <ny2485@naver.com>
2024-04-20 23:45:57 +0900

    fix : from Korean method name to English method name

--- src/test/java/spring/playground/basic/scope/PrototypeProviderTest.java
+++ src/test/java/spring/playground/basic/scope/PrototypeProviderTest.java
@@ -2,57 +2,34 @@ package spring.playground.basic.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest1 {
-
-    AnnotationConfigApplicationContext ac;
-
-    @BeforeEach
-    void beforeInit() {
-         ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
-    }
+public class PrototypeProviderTest {
 
     @Test
-    @DisplayName("프로토타입_찾기")
-    void findPrototype() {
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1); // 테스트 통과
-    }
-
-    @Test
-    @DisplayName("싱글톤_client가_프로토타입을_사용할 때")
-    void singletonClientUsePrototype() {
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
-
-    static class ClientBean { // 기본 : singleton
-        private final PrototypeBean prototypeBean;
-
+    static class ClientBean {
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
+        private ApplicationContext ac;
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -65,16 +42,13 @@ public class SingletonWithPrototypeTest1 {
         public void addCount() {
             count++;
         }
-
         public int getCount() {
             return count;
         }
-
         @PostConstruct
         public void init() {
             System.out.println("PrototypeBean.init " + this);
         }
-
         @PreDestroy
         public void destroy() {
             System.out.println("PrototypeBean.destroy");
