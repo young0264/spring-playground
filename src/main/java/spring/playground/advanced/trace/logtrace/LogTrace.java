374c0c6c4a259ddcf946bf3874ac20e24e5f7094
남의영 <ny2485@naver.com>
2024-05-07 08:54:51 +0900

    add LogTrace interface

---
+++ src/main/java/spring/playground/advanced/trace/logtrace/LogTrace.java
@@ -0,0 +1,12 @@
+package spring.playground.advanced.trace.logtrace;
+
+import spring.playground.advanced.trace.TraceStatus;
+
+public interface LogTrace {
+
+    TraceStatus begin(String message);
+
+    void end(TraceStatus status);
+
+    void exception(TraceStatus status, Exception e);
+}
