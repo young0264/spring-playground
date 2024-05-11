5f46a17c6faec4c164a1ba70c46680e2067039ff
남의영 <ny2485@naver.com>
2024-05-11 23:25:16 +0900

    add : new LogTrace using Field

---
+++ src/main/java/spring/playground/advanced/trace/logtrace/FieldLogTrace.java
@@ -0,0 +1,71 @@
+package spring.playground.advanced.trace.logtrace;
+
+import lombok.extern.slf4j.Slf4j;
+import spring.playground.advanced.trace.TraceId;
+import spring.playground.advanced.trace.TraceStatus;
+
+/** 파라미터 X, TraceId 동기화 */
+@Slf4j
+public class FieldLogTrace implements LogTrace {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    private TraceId traceIdHolder; //traceId 동기화, 동시성 이슈 발생
+
+
+    @Override
+    public TraceStatus begin(String message) {
+        return null;
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(),
+                    resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
+                    e.toString());
+        }
+        releaseTraceId();
+    }
+
+    private void syncTraceId() {
+        if (traceIdHolder == null) {
+            traceIdHolder = new TraceId();
+        } else {
+            traceIdHolder = traceIdHolder.createNextId();
+        }
+    }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) {
+            traceIdHolder = null; //destroy
+        } else {
+            traceIdHolder = traceIdHolder.createPreviousId();
+        } }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append( (i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+}
