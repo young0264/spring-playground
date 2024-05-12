7d42480adbac1dc7722d2f726e321dead321ddb5
남의영 <ny2485@naver.com>
2024-05-13 01:51:05 +0900

    fix from using TraceId field to using ThreadLocal

--- src/main/java/spring/playground/advanced/LogTraceConfig.java
+++ src/main/java/spring/playground/advanced/LogTraceConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.playground.advanced.trace.logtrace.FieldLogTrace;
 import spring.playground.advanced.trace.logtrace.LogTrace;
+import spring.playground.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+//        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
