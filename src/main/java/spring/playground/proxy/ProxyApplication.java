e18942c14154303b06998c074e89cadf711f6b11
남의영 <ny2485@naver.com>
2024-05-22 13:49:34 +0900

    add : applying dynamic proxy 1

--- src/main/java/spring/playground/proxy/ProxyApplication.java
+++ src/main/java/spring/playground/proxy/ProxyApplication.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import spring.playground.proxy.config.v1_proxy.ConcreteProxyConfig;
 import spring.playground.proxy.config.v1_proxy.InterfaceProxyConfig;
+import spring.playground.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import spring.playground.proxy.trace.logtrace.LogTrace;
 import spring.playground.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "spring.playground.proxy.app") //주의
 public class ProxyApplication {
 
