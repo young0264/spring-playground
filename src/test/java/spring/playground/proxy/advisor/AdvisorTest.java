97673827bf5b3a6af78ae8dd7fd9ef6c269ea052
남의영 <ny2485@naver.com>
2024-05-23 14:18:47 +0900

    add use one proxyfactory, multiple advisors

--- src/test/java/spring/playground/proxy/advisor/AdvisorTest.java
+++ src/test/java/spring/playground/proxy/advisor/AdvisorTest.java
@@ -1,16 +1,36 @@
 package spring.playground.proxy.advisor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import spring.playground.proxy.common.service.ServiceImpl;
 import spring.playground.proxy.common.service.ServiceInterface;
 import spring.playground.proxy.pureproxy.proxy.common.advice.TimeAdvice;
 
 @Slf4j
 public class AdvisorTest {
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷")
+    void advisorTest3() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("save");
+//        pointcut.setMappedNames("save", "find");
+//        pointcut.setMappedNames("find");
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new
+                TimeAdvice());
+
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        proxy.save();
+        proxy.find(); //setMappedNames에 넣어주지 않으면 find는 advisor가 적용되지 않음.
+    }
 
     @Test
     void advisorTest1() {
