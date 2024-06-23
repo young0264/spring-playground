41f334dc989f7f77a78a45b50dc2a5b7c482c490
남의영 <ny2485@naver.com>
2024-06-23 23:11:30 +0900

    changes in origin branch1

---
+++ src/test/java/spring/playground/rebase/RebaseBranchOrigin1.java
@@ -0,0 +1,14 @@
+package spring.playground.rebase;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class RebaseBranchOrigin1 {
+
+    @Test
+    void originBranch1() {
+        log.info("Changes in origin branch1");
+    }
+
+}
